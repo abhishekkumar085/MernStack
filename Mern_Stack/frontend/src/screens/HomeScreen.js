b50afef2224e2c0f5402b496950e089bb359fbec
Abhishek Kumar <abhishekkumar08528@gmail.com>
2023-05-15 15:28:29 +0530

    loading box integrated

--- Mern_Stack/frontend/src/screens/HomeScreen.js
+++ Mern_Stack/frontend/src/screens/HomeScreen.js
@@ -1,15 +1,42 @@
 import { Link } from 'react-router-dom';
 // import data from '../data';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer,  } from 'react';
 import axios from 'axios';
 
+
+const reducer=(state,action)=>{
+  switch(action.type){
+    case 'FETCH_REQUEST':
+      return{...state,loading:true}
+
+    case 'FETCH_SUCCESS':
+      return{...state,products:action.payload,loading:false};
+    case 'FETCH_FAIL':
+      return{...state,loading:false,error:action.payload};
+    
+    default:
+      return state;
+
+  }
+};
+
 function HomeScreen() {
-  const[products,setProducts]=useState([]);
+  const [{loading,error,products},dispatch]=useReducer(reducer,{
+    products:[],
+    loading:true,error:'',});
+  // const[products,setProducts]=useState([]);
 
   useEffect(()=>{
 const fetchData=async ()=>{
-  const result = await axios.get('/api/products');
-  setProducts(result.data);
+  dispatch({type:'FETCH_REQUEST'});
+  try{
+    const result = await axios.get('/api/products');
+    dispatch({type:'FETCH_SUCCESS',payload:result.data});
+  }catch(err){
+    dispatch({type:'FETCH_FAIL',payload:err.message});
+  }
+ 
+  // setProducts(result.data);
 }
 fetchData();
   },[])
@@ -17,14 +44,22 @@ fetchData();
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {products.map((product) => (
+      {loading?(
+              <div>Loading....</div>
+            ):error?(
+              <div>{error}</div>
+            ):(
+              
+            
+        products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
             </Link>
             <div className="product-info">
               <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
+                {/* <p>{product.name}</p> */}
+                <img src={product.image} alt={product.name}/>
               </Link>
               <p>
                 <strong>${product.price}</strong>
@@ -32,7 +67,8 @@ fetchData();
               <button>Add to cart</button>
             </div>
           </div>
-        ))}
+            ))
+            )}
       </div>
     </div>
   );
